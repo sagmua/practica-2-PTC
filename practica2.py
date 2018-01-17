b369285ae82c34f157be4f91682125914aa39bfe
Samuel Cardenete <samuel@MacBook-Pro-de-Juan.local>
2018-01-17 18:26:16 +0100

    interfaz definida

---
+++ practica2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 16 17:12:00 2018
+
+@author: samuel
+"""
+
+
+from test import Test #La clase de nuestro examen
+from tkinter import * #Para la interfaz
+from math import *
+
+
+if __name__ == "__main__":
+    
+    
+    t = Test()
+    
---
+++ test.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 16 17:15:53 2018
+
+@author: samuel
+"""
+
+class Test:
+    def __init__(self, nombre_test = ''):
+        self.nombre_test = nombre_test
+        self.num_preguntas = 0
+        self.preguntas = ['hola']
+        self.respuestas = []
+        
+        
+    def aniadir_pregunta(self, texto):
+        self.preguntas.append(texto)
+        self.num_preguntas += 1
+    
+    def aniadir_respuesta(self, texto, valoracion):
+        respuesta = (texto, valoracion)
+        
+        if len(self.respuestas) == self.num_preguntas:
+            self.respuestas.append([respuesta])
+        else:
+            self.respuestas[self.num_preguntas].append(respuesta)
+            
+    
+    def print_respuestas(self):
+        for i in self.respuestas:
+            print(i)
+    def print_preguntas(self):
+        for i in self.preguntas:
+            print(i)
+        
+
+
+if __name__ == '__main__':
+    
+    test = Test('el primero')
+    
+    
